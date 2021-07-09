5ba592b8973591b6f9a408e9e3c0a85fe11358a8
Victor Amit <44795913+VictorAmit@users.noreply.github.com>
2021-07-09 20:53:02 +0530

    Y

---
+++ serverless.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Require the framework
+import Fastify from "fastify";
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: true,
+});
+
+// Register your application as a normal plugin.
+app.register(import("./app.js"));
+
+export default async (req, res) => {
+    await app.ready();
+    app.server.emit('request', req, res);
+}
